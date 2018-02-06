d8ad302d3f6b2b3f84ea4c8ccf72b5d2504972b1
Thomas Luinaud <thomas.luinaud@polymtl.ca>
2018-02-05 19:25:57 -0500

    code clean-up

--- compiler.scm
+++ compiler.scm
@@ -26,42 +26,6 @@
     (lambda (port)
       (parse-exprs port))))
 
-;(define (parse-exprs port)
-; (let ((x (read port)))
-;    (if (eof-object? x)
-;        '()
-;        (cons x
-;              (parse-exprs port)))))
-
-;;;----------------------------------------------------------------------------
-
-;; Program compiler.
-
-(define (compile-program exprs)
-  (list " .text\n"
-        " .globl _main\n"
-        " .globl main\n"
-        "_main:\n"
-        "main:\n"
-        (map compile-expr exprs)
-        " mov $0, %rax\n"
-        " ret\n"))
-
-;(define (compile-expr expr)
-;  (if (and (pair? expr)
-;           (equal? (car expr) 'println)
-;           (pair? (cdr expr))
-;           (number? (cadr expr))
-;           (null? (cddr expr)))
-
-;      (let ((num (cadr expr)))
-;        (list " push $" num "\n"
-;              " call print_word_dec\n"
-;              " push $10\n"
-;              " call putchar\n"))
-;
-;      (error "unknown expression" expr)))
-
 ;;;----------------------------------------------------------------------------
 
 (define gcc-link-option
