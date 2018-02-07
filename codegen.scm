bb051ab4f9c94fb2742304c3280ff16de0ccc672
Thomas Luinaud <thomas.luinaud@polymtl.ca>
2018-02-07 11:53:23 -0500

    debut de modificaiton avec debut de support pour le une fonction du genre (printl (+ 45 56))

--- codegen.scm
+++ codegen.scm
@@ -1,20 +1,33 @@
 ;fichier pour la génération du code assembleur
 
 (define (compile-expr expr)
-  (if (and (pair? expr)
-           (equal? (car expr) 'println)
-           (pair? (cdr expr))
-           (number? (cadr expr))
-           (null? (cddr expr)))
-
-      (let ((num (cadr expr)))
-        (list " push $" num "\n"
-              " call print_word_dec\n"
-              " push $10\n"
-              " call putchar\n"))
-      (error "unknown expression" expr))
-)
-
+  (if (and (pair? expr))
+      (if (and (pair? (cadr expr)))
+	  (compile-expr (cadr expr))
+	  (let ((num (cadr expr)))
+	    (append (list " push $" num "\n")
+		    (analyse-op (car expr)))))
+	  (error "unknown expression" expr))
+  )
+;fonction pour analyser une opération (premier élément d'une parenthèse)
+(define (analyse-op expr)
+  (pp expr)
+  (if (equal?  expr 'println)
+      (list  " call print_word_dec\n"
+             " push $10\n"
+             " call putchar\n")
+      (if (equal? expr '+)
+	  (list " pop %rax\n"
+		" pop %rbx \n "
+	        " add %rax, %rbx \n"
+		" push %rbx \n"
+		)
+       )
+      (error "unknown operation" expr)
+      )
+  )
+(trace compile-expr)
+(trace analyse-op)
 (define (compile-program exprs)
   (list " .text\n"
         " .globl _main\n"
