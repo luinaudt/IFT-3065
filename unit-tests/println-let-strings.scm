1dd88d1ac1c5a73ba917fa894dbdd7bc46ac0fb2
Francis de Ladurantaye <fdl9044@gmail.com>
2018-02-16 22:44:14 -0500

    reste à corriger les fautes du rapport

--- unit-tests/println-let-strings.scm
+++ unit-tests/println-let-strings.scm
@@ -1,8 +1,8 @@
 (println (let ((x "esr") (y "ert") (z "fin"))
-;	   (println x)
-;	   (println y)
-;	   (println z)
-  5))
+	   (println x)
+	   (println y)
+	   (println z)
+           5))
 
 ;esr
 ;ert
