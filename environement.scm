85a2c3316edfb71484d081825b15ed13fed67d5a
Thomas Luinaud <thomas.luinaud@polymtl.ca>
2018-03-08 10:17:21 -0500

    ajout du fichier pour gérer l'environnement

---
+++ environement.scm
@@ -0,0 +1,52 @@
+;; fichier pour la gestion de l'environnement.
+
+;;environnement de compilation
+(define gcte '())
+;;environnement d'exécution
+(define grte '())
+
+;;recherche dans l'enironnement
+(define (env-lookup env var)
+  (let ((x (assq var env)))
+    (if x (cdr x) (error "unbound " var))))
+
+
+;;extension de l'environnement pour les lambda
+(define (cte-extend cte vars) (append vars cte))
+(define (rte-extend cte vars) (append vars r0te))
+
+
+;;fonction de recherche
+(define (cte-lookup cte var)
+  (let ((x (memq var cte)))
+    (if x
+	(- (length cte) (length x))
+	(error "unbound " var))))
+
+(define (rte-lookup rte pos)
+  (list-ref rte pos))
+
+;;fonction pour l'evaluation
+(define (ev expr cte rte)
+  (match expr
+	 (,const when (constant? const)
+		 const)
+
+	 (,var when (variable? var)
+	       (let ((pos (cte-lookup cte var)))
+		 (rte-lookup rte pos))
+
+	 ((lambda ,param ,body)
+	  (lambda args (ev body (cte-extend cte params)
+			        (rte-extend rte args))))
+
+	 ((,fun . ,exprs)
+	  (apply (ev fun cte rte)
+		 (map (lambda (x) (ev x cte rte))
+		      exprs)))))
+
+(define (eval expr)
+  (ev expr gcte grte))
+
+
+
