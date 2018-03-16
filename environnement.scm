46022804c66827bc27eb9706b04709962ac6e1bd
Francis de Ladurantaye <fdl9044@gmail.com>
2018-03-16 16:20:46 -0400

    environnement pour compilation seulement, pas d'interprétation ^_^

--- environnement.scm
+++ environnement.scm
@@ -1,7 +1,5 @@
 (include "match.scm")
 
-(define genv '())
-
 (define (ev expr env)
   (match expr
          
@@ -12,11 +10,11 @@
                (env-lookup env var))
 
 	 ((lambda ,params ,body)
-	  (lambda args (ev body env)))
+	  (lambda args (ev body (env-extend env params args))))
 
          ((,fun . ,exprs)
-          (apply (ev fun)
-                 (map (lambda (x) (ev x))
+          (apply (ev fun env)
+                 (map (lambda (x) (ev x env))
                       exprs)))))
 
 (define (env-lookup env var)
@@ -39,70 +37,3 @@
 
 (define (eval expr)
   (ev expr genv))
-
-
-
-;; ;; fichier pour la gestion de l'environnement.
-;; (include "match.scm")
-;; ;;environnement de compilation
-;; (define gcte '());; prims)
-;; ;;environnement d'exécution
-;; (define grte '());;prims)
-
-;; ;;recherche dans l'enironnement
-;; (define (env-lookup env var)
-;;   (let ((x (assq var env)))
-;;     (if x (cdr x) (error "unbound " var))))
-
-
-;; ;;extension de l'environnement pour les lambda
-;; (define (cte-extend cte vars) (append vars cte))
-;; (define (rte-extend rte vars) (append vars rte))
-
-
-;; ;;fonction de recherche
-;; (define (cte-lookup cte var)
-;;   (let ((x (memq var cte)))
-;;     (if x
-;; 	(- (length cte) (length x))
-;; 	(error "unbound " var))))
-
-;; (define (rte-lookup rte pos)
-;;   (list-ref rte pos))
-
-;; (define (constant? x)
-;;   (or (number? x)
-;;       (string? x)
-;;       (boolean? x)))
-;; (define (variable? x)
-;;   (symbol? x))
-
-;; ;;fonction pour l'evaluation
-;; (define (ev expr cte rte)
-;;   (match expr
-;; 	 (,const when (constant? const)
-;; 		 const)
-
-;; 	 (,var when (variable? var)
-;; 	       (let ((pos (cte-lookup cte var)))
-;; 		 (rte-lookup rte pos)))
-
-;; 	 ((lambda ,params ,body)
-;; 	  (lambda args (ev body (cte-extend cte params)
-;; 			   (rte-extend rte args))))
-
-;; 	 ((,fun . ,exprs)
-;; 	  (apply (ev fun cte rte)
-;; 		 (map (lambda (x) (ev x cte rte))
-;; 		      exprs)))))
-
-;; (define (eval expr)
-;;   (ev expr gcte grte))
-
-;; ;;(trace eval)
-;; ;;(trace ev)
-;; ;;(trace rte-lookup)
-;; ;;(trace cte-lookup)
-;; ;;(trace env-lookup)
-;; ;;(trace cte-extend)
-;; ;;(trace rte-extend)
