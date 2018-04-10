7aadf2aa10b8b8b973be376a3b68388222c2ea1f
Thomas Luinaud <thomas.luinaud@polymtl.ca>
2018-04-10 00:18:21 -0400

    création assignation d'une fonction

--- unit-tests/etape3-let-multipleCall.scm
+++ unit-tests/etape3-let-multipleCall.scm
@@ -1,7 +1,11 @@
 (let ((x 12)) (println x) (println 15))
-(let ((x 56) (y 45) (x 5)) (println (+ x y)) (println x) (println y))
+(let ((x 56) (y 45)) (println (+ x y)) (println x) (println y))
+(let ((t println) (x 55) (y 45)) (t 10) (t x) (t (+ x y)))
 ;12
 ;15
 ;101
 ;56
 ;45
+;10
+;55
+;100
