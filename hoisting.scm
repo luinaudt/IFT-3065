641ea7dd50dd3c3dc605d191b274894fca81a923
Francis de Ladurantaye <fdl9044@gmail.com>
2018-04-11 00:19:22 -0400

    Revert "generation tout comme il faut"
    This reverts commit 2958592498367a0b111fb81368d08cd966075e27.

--- hoisting.scm
+++ hoisting.scm
@@ -1,12 +1,5 @@
 ;;hoixting for closure
 ;;(define new-define '())
-
-(define (hoist-closure-2 expr)
-  (if (null? expr)
-      '()
-      (append (hoist-closure (car expr))
-	    (hoist-closure-2 (cdr expr))))) 
-
 (define (hoist-closure expr)
   (let* ((new-define '())
 	 (new-code
@@ -18,10 +11,10 @@
 				 ((make-closure ,code . ,fv)
 				  (let ((sym (gensym)))
 				    (begin
-				      (let ((to-append (list (hoist-closing `(define ,sym ,code)))))
-					(set! new-define (if (null? new-define)
-							     to-append
-							     (append new-define to-append)))) 
+				      (set! new-define (if (null? new-define)
+							   (list (hoist-closing `(define ,sym ,code)))
+							   (append new-define (list (hoist-closing `(define ,sym ,code)))
+							     )))
 				      `(make-closure ,sym ,@fv))
 				    
 				    ;;		(map hoist-closure code)
@@ -38,6 +31,5 @@
 				  `(,E0 ,@(map hoist-closing E1))
 				  ))))))
 	    (hoist-closing expr))))
-    (append new-define (list new-code))))
+    (append new-define new-code)))
 ;;(trace hoist-closure)
-;;(trace hoist-closure-2)
