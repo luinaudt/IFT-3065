27e0198def5f10c32ccdd645885a322a895b6332
Francis de Ladurantaye <fdl9044@gmail.com>
2018-04-11 17:01:05 -0400

    char->integer et integer->char fonctionnels

--- lib.scm
+++ lib.scm
@@ -17,8 +17,8 @@
 
 ;(define read-char     (lambda ()  ($read-char)))
 ;(define write-char    (lambda (x) ($write-char x)))
-;(define integer->char (lambda (x) ($integer->char x)))
-;(define char->integer (lambda (x) ($char->integer x)))
+(define integer->char (lambda (x) ($integer->char x)))
+(define char->integer (lambda (x) ($char->integer x)))
 (define char?         (lambda (x) ($char? x)))
 
 ;(define make-string   (lambda (x)   ($make-string x)))
