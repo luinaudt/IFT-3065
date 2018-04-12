525884bd44dbea76f2b41ae1e8dc56616a29c72c
thomas Luinaud <thomas.luinaud@gmail.com>
2018-04-12 19:13:31 -0400

    resolu

--- unit-tests/etape3-closure-set.scm
+++ unit-tests/etape3-closure-set.scm
@@ -1,3 +1,10 @@
+(define test2
+  (lambda (n)
+    (lambda (x)
+      (begin
+	(set! n (+ n x))
+	n))))
+
 (define test
   (lambda (n)
     (cons (lambda (x)
@@ -9,13 +16,16 @@
 	      (set! n (- n x))
 	      n)))))
 
+(define ts (test2 0))
 (define p (test 0))
 (define inc (car p))
 (define dec (cdr p))
 
-(inc 3)
-(inc 3)
-(dec 1)
+(println (ts  1))
+(println (inc 3))
+(println (inc 3))
+(println (dec 1))
+;1
 ;3
 ;6
 ;5
