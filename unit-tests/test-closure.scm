fa9c40713b3093946146c41c05dcabd0d22635eb
thomas Luinaud <thomas.luinaud@gmail.com>
2018-04-12 02:21:48 -0400

    ca fonctionne pour des variables non mutables

--- unit-tests/test-closure.scm
+++ unit-tests/test-closure.scm
@@ -1,13 +1,12 @@
 (define f
   (lambda (n y)
     (lambda (x) ($+ n x))))
-;;(define g (f 5 6))
+(define g (f 5 6))
 (define g2 (lambda (x) ((f 5 6) x)))
-;;(define h (f 42 43))
+(define h (f 42 43))
 (println (g2 10))
-;;(println (g 10))
-
-
-
-
-
+(println (g 10))
+(println (h 5))
+;15
+;15
+;47
