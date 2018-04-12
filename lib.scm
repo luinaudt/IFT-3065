c8d2602a94853bfbec5e0099f7af267500a20165
thomas Luinaud <thomas.luinaud@gmail.com>
2018-04-12 14:04:04 -0400

    ajout vector base fonctions

--- lib.scm
+++ lib.scm
@@ -36,6 +36,10 @@
 
 (define procedure? (lambda (x)   ($procedure? x)))
 (define eq?        (lambda (x y) ($eq? x y)))
+(define make-vector (lambda (l d) ($make-vector l d)))
+(define vector-length (lambda (x) ($vector-length x)))
+(define vector-ref (lambda (vec p) ($vector-ref vec p)))
+(define vector-set! (lambda (vec p o) ($vector-set! vec p o)))
 
 ;;;============================================================================
 
@@ -58,7 +62,7 @@
           #f)))
 
  (define eqv?
-  (lambda (x y)
+   (lambda (x y)
      (if (and ($number? x) ($number? y))
          ($= x y)
          ($eq? x y))))
