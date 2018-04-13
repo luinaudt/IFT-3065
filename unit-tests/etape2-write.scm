d4ef17baef565403112e03e4ee88884f7aeab655
thomas Luinaud <thomas.luinaud@gmail.com>
2018-04-12 22:12:32 -0400

    test pour write

---
+++ unit-tests/etape2-write.scm
@@ -0,0 +1,36 @@
+(write-char #\5)
+(write-char #\newline)
+($write-char #\5)
+(write-char #\newline)
+(write #\5)
+(write-char #\newline)
+(write "bonjour")
+(write-char #\newline)
+(write -5)
+(write-char #\newline)
+(write 125)
+(write-char #\newline)
+(write 9)
+(write-char #\newline)
+(write -145)
+(write #f)
+(write #t)
+(write-char #\newline)
+(write '(4 5 "456" 9))
+(write-char #\newline)
+(write '(4 5 "456" (98 89 9 "hjds")))
+(write-char #\newline)
+;(write 'bonbon)
+;(write-char #\newline)
+
+
+;5
+;5
+;#\5
+;"bonjour"
+;-5
+;125
+;9
+;-145#f#t
+;(4 5 "456" 9)
+;(4 5 "456" (98 89 9 "hjds"))
