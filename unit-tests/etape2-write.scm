1a52eb3d51fd75e02ed192bf9bbd772d6cce96a2
thomas Luinaud <thomas.luinaud@gmail.com>
2018-04-13 12:28:20 -0400

    test pour le \n dans une chaine de caracteres

--- unit-tests/etape2-write.scm
+++ unit-tests/etape2-write.scm
@@ -18,13 +18,15 @@
 (write-char #\newline)
 (write '(4 5 "456" 9))
 (write-char #\newline)
-(write '(4 5 "456" (98 89 9 "hjds")))
+(write '(4 #\5 5 "456" (98 89 9 "hjds")))
 (write-char #\newline)
 (write '( ))
 (write-char #\newline)
 (write 'bonbon)
 (write-char #\newline)
 (write println)
+(write-char #\newline)
+(write "bonjour\n hello")
 (write-char #\newline)
 ;5
 ;5
@@ -35,7 +37,8 @@
 ;9
 ;-145#f#t
 ;(4 5 "456" 9)
-;(4 5 "456" (98 89 9 "hjds"))
+;(4 #\5 5 "456" (98 89 9 "hjds"))
 ;()
 ;bonbon
 ;#<procedure >
+;"bonjour\n hello"
