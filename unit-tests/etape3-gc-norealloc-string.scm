c4d270f728b85a296cc267fe2ae5e549bdf3ae2e
thomas Luinaud <thomas.luinaud@gmail.com>
2018-04-16 14:01:59 -0400

    modification macros avec adaptation tests unitaires pour eviter les stack overflow

--- unit-tests/etape3-gc-norealloc-string.scm
+++ unit-tests/etape3-gc-norealloc-string.scm
@@ -1,12 +1,12 @@
 (define t 0)
 (let loop2 ((y 1))
-  (if (< 150000 y)
+  (if (< 100000 y)
       (println "fin string symbole")
       (begin
 	(set! t '"untexterandomquejevaisafficherpleindefois")
 	(loop2 (+ y 1)))))
 (let loop2 ((y 1))
-  (if (< 150000 y)
+  (if (< 100000 y)
       (println "fin string")
       (begin
 	(set! t "untexterandomquejevaisafficherpleindefois")
